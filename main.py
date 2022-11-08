f8cbdde7479ac90d9371859b01a8189a57f80af2
mursalimdsc <mursalim.dsc@gmail.com>
2022-11-08 15:40:18 +0700

    Menambahkan file main.py dan projek spyproject

---
+++ main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  8 15:34:22 2022
+
+@author: salim
+"""
+# Halaman main
+
+def hello():
+    return print("Hello world")
+
+
+def perkalian(a,b):
+    nilai_a = a
+    nilai_b = b
+    
+    hitung_perkalian = nilai_a * nilai_b
+    
+    return hitung_perkalian
+
+
+# cetak hasil output
+print(perkalian(4,5))
+hello()
+    
